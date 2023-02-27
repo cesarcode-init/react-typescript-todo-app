fbfcee11c4a563e8cb87c6015915b6412b8382cc
cesarcode-init <kaissar.mouelhi@outlook.com>
2023-02-27 17:23:45 +0100

    chore: create todos state reducer

--- src/contexts/TodosContext.tsx
+++ src/contexts/TodosContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const TodosContext = createContext(null);
 
@@ -6,6 +6,57 @@ type TodosProviderType = {
   children: React.ReactNode;
 };
 
+type ArrayOfType<T> = T[];
+
+interface Todo {
+  _id: number;
+  todo: string;
+  completed: boolean;
+}
+
+const enum ACTION_TYPES {
+  ADD_TODO,
+  DELETE_TODO,
+  UPDATE_TODO,
+}
+
+type State = ArrayOfType<Todo>;
+
+type Action =
+  | {
+      type: ACTION_TYPES.ADD_TODO;
+      payload: {
+        todo: string;
+      };
+    }
+  | {
+      type: ACTION_TYPES.DELETE_TODO;
+      payload: {
+        _id: number;
+      };
+    };
+
+const initialState: State = [];
+
+const reducer = (state: State, { type, payload }: Action) => {
+  switch (type) {
+    case ACTION_TYPES.ADD_TODO:
+      return [
+        ...state,
+        {
+          _id: Date.now(),
+          todo: payload.todo,
+          completed: false,
+        },
+      ];
+
+    default:
+      return state;
+  }
+};
+
 export const ProvideTodos = ({ children }: TodosProviderType): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return <TodosContext.Provider value={null}>{children}</TodosContext.Provider>;
 };
