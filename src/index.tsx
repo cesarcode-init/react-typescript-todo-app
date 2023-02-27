ce5ad40bf235de7728277e9cf788492353242c47
cesarcode-init <kaissar.mouelhi@outlook.com>
2023-02-27 16:13:11 +0100

    chore: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App/App';
 
+import './styles/globals.css';
+
 const rootElement = document.getElementById('root') as HTMLElement;
 
 const root = ReactDOM.createRoot(rootElement);
