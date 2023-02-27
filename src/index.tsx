6e9cc45b696a95e364525c49e21c672c13fee2d5
cesarcode-init <kaissar.mouelhi@outlook.com>
2023-02-27 16:29:45 +0100

    chore: set base url path

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App/App';
 
-import './styles/globals.css';
+import 'styles/globals.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,4 +1,4 @@
-import Todos from '../../components/Todos/Todos';
+import Todos from 'components/Todos/Todos';
 
 const App = (): JSX.Element => {
   return <Todos />;
