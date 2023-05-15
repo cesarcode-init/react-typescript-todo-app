b3508870a7202bb3eb149e244da0a42bc4b0d8c1
cesarcode-init <kaissar.mouelhi@outlook.com>
2023-05-15 23:21:13 +0100

    chore: add a footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { ProvideTodos } from 'contexts/TodosContext';
 
 import Todos from 'components/Todos/Todos';
+import Footer from 'components/Footer/Footer';
 
 const App = (): JSX.Element => {
   return (
     <ProvideTodos>
       <Todos />
+
+      <Footer />
     </ProvideTodos>
   );
 };
